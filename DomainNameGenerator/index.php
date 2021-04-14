578588f5743e663ccf24d174276f91361bacbf79
allexgalbert <allexgalbert@gmail.com>
2021-04-14 23:46:32 +0300

    update workflow

--- DomainNameGenerator/index.php
+++ DomainNameGenerator/index.php
@@ -8,7 +8,7 @@ class Getdomain {
     $neutral = file('neutral', FILE_USE_INCLUDE_PATH | FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     $theme = file('theme' . $theme_id, FILE_USE_INCLUDE_PATH | FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-    $rands = array(
+    $rands = [
 
       //нейтральное слово + тематическое слово
       trim($neutral[array_rand($neutral)]) . trim($theme[array_rand($theme)]),
@@ -27,7 +27,7 @@ class Getdomain {
 
       //тематическое слово + - + тематическое слово
       trim($theme[array_rand($theme)]) . '-' . trim($theme[array_rand($theme)]),
-    );
+    ];
 
     return trim($rands[array_rand($rands)]);
   }
